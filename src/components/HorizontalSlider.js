767cec0dc79b2e00c34cf22b361f2eaa6b836684
Adrien Lacroix <adrienla@theodo.fr>
2017-08-01 00:39:18 +0200

    :sparkles: add HorizontalSlider component

---
+++ src/components/HorizontalSlider.js
@@ -0,0 +1,36 @@
+// @flow
+
+import React from 'react';
+import { StyleSheet, ScrollView, View } from 'react-native';
+import appStyle from 'bakareader/src/appStyle';
+
+const styles = StyleSheet.create({
+  wrapper: {
+    marginTop: 32,
+    marginBottom: appStyle.grid.x2,
+  },
+  content: {
+    paddingLeft: appStyle.grid.x2,
+    paddingRight: appStyle.grid.x2,
+  },
+});
+
+type PropsType = {
+  children: Array<any>
+}
+
+function HorizontalSlider(props: PropsType) {
+  return (
+    <View style={styles.wrapper}>
+      <ScrollView
+        horizontal
+        contentContainerStyle={styles.content}
+        showsHorizontalScrollIndicator={false}
+      >
+        {props.children}
+      </ScrollView>
+    </View>
+  );
+}
+
+export default HorizontalSlider;
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@
 export { default as Page } from './Page';
 export { default as Button } from './Button';
 export { default as Card } from './Card';
+export { default as HorizontalSlider } from './HorizontalSlider';
