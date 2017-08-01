7718d13aa639f09259cefb0300696a93c110c3a0
Adrien Lacroix <adrienla@theodo.fr>
2017-08-01 19:46:20 +0200

    :construction: add Grid to homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
-import { Page, Button, Card, HorizontalSlider } from 'bakareader/src/components';
+import { Button, Card, Grid, HorizontalSlider, Page } from 'bakareader/src/components';
 import appStyle from 'bakareader/src/appStyle';
 
 const styles = StyleSheet.create({
@@ -12,11 +12,6 @@ const styles = StyleSheet.create({
     fontSize: appStyle.font.size.huge,
     marginTop: appStyle.grid.x2,
     marginLeft: appStyle.grid.x2,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: appStyle.colors.darkGray,
-    marginBottom: appStyle.grid.x1,
   },
 });
 
@@ -38,19 +33,21 @@ class Home extends Component {
     return (
       <Page noMargin>
         <View style={styles.container}>
-          <Text style={styles.header}>
-            En cours
-          </Text>
-          <HorizontalSlider>
-            <Card coverUri={require('bakareader/assets/images/dummy/cover1.jpg')} title="Nisekoi Tome 1" />
-            <Card coverUri={require('bakareader/assets/images/dummy/cover2.jpg')} title="Nisekoi Tome 2" />
-            <Card coverUri={require('bakareader/assets/images/dummy/cover3.jpg')} title="Nisekoi Tome 3" />
-          </HorizontalSlider>
-          <Text style={styles.instructions}>
-            Double tap R on your keyboard to reload,{'\n'}
-            Shake or press menu button for dev menu
-          </Text>
-          <Button onPress={this._goToInfos}>Go to the Infos page</Button>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <Text style={styles.header}>
+              En cours
+            </Text>
+            <HorizontalSlider>
+              <Card coverUri={require('bakareader/assets/images/dummy/cover1.jpg')} title="Nisekoi Tome 1" />
+              <Card coverUri={require('bakareader/assets/images/dummy/cover2.jpg')} title="Nisekoi Tome 2" />
+              <Card coverUri={require('bakareader/assets/images/dummy/cover3.jpg')} title="Nisekoi Tome 3" />
+            </HorizontalSlider>
+            <Button onPress={this._goToInfos}>Go to the Infos page</Button>
+            <Text style={styles.header}>
+              Collection
+            </Text>
+            <Grid />
+          </ScrollView>
         </View>
       </Page>
     );
