05d8ffc279b248c1372c61d4b6ca29ca67870e76
Adrien Lacroix <adrienla@theodo.fr>
2017-08-01 21:39:01 +0200

    :construction: using infos as a reader screen

--- src/pages/Infos/index.js
+++ src/pages/Infos/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Book, Page } from 'bakareader/src/components';
+import { StatusBar, StyleSheet, View } from 'react-native';
+import { Book, Page, TouchableIcon } from 'bakareader/src/components';
 
 const styles = StyleSheet.create({
   container: {
@@ -19,20 +19,64 @@ const IMAGES = [
 ];
 
 class Infos extends Component {
-  static navigationOptions = {
-    title: 'Infos',
+  static navigationOptions = ({ navigation }) => {
+    const { state, setParams } = navigation;
+    const isHidden = state.params ? state.params.isHidden : false;
+    if (isHidden) {
+      return { header: null, gesturesEnabled: false };
+    }
+
+    return {
+      title: 'Reader',
+      headerRight: (
+        <TouchableIcon
+          name="fullscreen"
+          onPress={() => setParams({ isHidden: true })}
+        />
+      ),
+    };
   };
+
+  componentWillMount() {
+    this.handlePress = this.handlePress.bind(this);
+    this.setState({
+      lastPress: 0,
+    });
+  }
+
   props: PropsType;
 
+  handlePress: Function;
+
+  toggleNavigationBar() {
+    const { state, setParams } = this.props.navigation;
+    const isHidden = state.params ? state.params.isHidden : false;
+    setParams({ isHidden: !isHidden });
+  }
+
+  handlePress() {
+    const delta = new Date().getTime() - this.state.lastPress;
+    if (delta < 200) { // DOUBLE TAP
+      this.toggleNavigationBar();
+    }
+    this.setState({
+      lastPress: new Date().getTime(),
+    });
+  }
+
   render() {
+    const { params } = this.props.navigation.state;
+    const isStatusBarHidden = params ? params.isHidden : false;
     return (
       <Page noMargin noNavBar>
+        <StatusBar hidden={isStatusBarHidden} />
         <View style={styles.container}>
           <Book
             currentPage={1}
             images={IMAGES}
             rtl
             totalPages={IMAGES.length}
+            onPress={this.handlePress}
           />
         </View>
       </Page>
