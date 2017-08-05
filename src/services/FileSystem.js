c7172546321fe0ce04a38ec674de44c76a820130
Adrien Lacroix <adrienla@theodo.fr>
2017-08-05 22:00:21 +0200

    :sparkles: add FileSystem service

---
+++ src/services/FileSystem.js
@@ -0,0 +1,45 @@
+import RNFetchBlob from 'react-native-fetch-blob';
+
+const DIRECTORIES = RNFetchBlob.fs.dirs;
+
+function isRawBook(file) {
+  return file.type === 'directory';
+}
+
+function isImage(file) {
+  return file.filename.split('.').pop() === 'jpg';
+}
+
+export function getParentPath(path) {
+  return path.substring(0, path.lastIndexOf('/') + 1);
+}
+
+const BOOKS_DIR = `${getParentPath(DIRECTORIES.DocumentDir)}Books`;
+
+export async function createBooksDirectory() {
+  const isAlreadyPresent = await RNFetchBlob.fs.isDir(BOOKS_DIR);
+
+  if (!isAlreadyPresent) {
+    await RNFetchBlob.fs.mkdir(BOOKS_DIR);
+  }
+}
+
+export async function getUserImportedBooks() {
+  const files = await RNFetchBlob.fs.lstat(DIRECTORIES.DocumentDir);
+  return files.filter(file => isRawBook(file));
+}
+
+export async function getDirectoryFiles(dirPath) {
+  return RNFetchBlob.fs.lstat(dirPath).then(files => files);
+}
+
+export async function formatBook(book) {
+  const path = `${BOOKS_DIR}/${book.filename}`;
+  const pagesPath = `${path}/pages`;
+  await RNFetchBlob.fs.mkdir(path);
+  await RNFetchBlob.fs.cp(book.path, pagesPath);
+  const files = await getDirectoryFiles(pagesPath);
+  const images = files.filter(file => isImage(file));
+  await Promise.all(images.map((image, index) => RNFetchBlob.fs.mv(image.path, `${pagesPath}/${index + 1}.jpg`)));
+  console.log('done');
+}
